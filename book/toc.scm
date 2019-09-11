e8c763ec57163bf10d4fc6266f18ea2cc11117f6
Liang Wang <liang.wang@nokia.com>
2019-09-12 00:41:19 +0300

    editing book ...

--- book/toc.scm
+++ book/toc.scm
@@ -4,15 +4,27 @@
  ((title "Numerical Techniques")
   (chapters
    (introduction
+    maths
+    stats
     ndarray
+    matrix
     algodiff
     optimisation
+    linear-algebra
+    diffequation
     neural-network
+    neural-ode
     visualisation
+    dataframe
+    nlp
     ))))
 
 (part
  ((title "System Architecture")
   (chapters
-   (
+   (architecture
+    cgraph
+    constant
+    zoo
+    backend
     ))))
